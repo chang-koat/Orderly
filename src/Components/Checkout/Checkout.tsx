cf6e29ff6dc8fd7f47b3a2b269ee7fd7fcc18733
RedThunder1 <redthunderspartan@gmail.com>
2025-11-19 12:25:39 -0600

    Simple store and cart pages are working, however they need some more work.

--- src/Components/Checkout/Checkout.tsx
+++ src/Components/Checkout/Checkout.tsx
@@ -1,42 +1,80 @@
 import './Checkout.css'
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Checkout() {
     const [items, setItems] = useState<any[]>([]);
 
     const cart = JSON.parse(sessionStorage.getItem("cart") as string);
 
-
     useEffect(() => {
         if (cart === null) {
             setItems([]);
         } else {
+            let price: number = 0
+            for (let item of cart) {
+                price += item.price
+                console.log("new item")
+                let options: any[] = []
+
+                for (let option of Object.entries(item.customizations)) {
+                    if (option.length === 2) {//Customzations are a little messed up at them moment so this is needed
+                        if (option[1] === "1") {
+                            console.log("option true bool", option, "adding", option[0], options)
+                            options.push(option[0])
+                        } else if (option[1] === "0") {
+                            console.log("option false bool", option, options);
+                            //do nothing for now
+                        } else {
+                            console.log("option mult choice", option, "adding", option[1], options);
+                            options.push(option[1])
+                        }
+                    }
+                    console.log(options)
+                }
+                item.customizations = options
+            }
             setItems(cart);
+            document.getElementById("checkout_price")!!.innerText = price.toString();
         }
+
+        
     },[])
 
-    const itemsMap = items?.map((item: any, index: number) => (
-        <div key={index} className="Checkout_item">
+    function removeFromCart(item: any, index: number, event: any) {
+        //Maybe set Prompt if the user is sure they want to delete the item.
 
-        </div>));
+        const cart = JSON.parse(sessionStorage.getItem("cart") as string);
+        cart.splice(index, 1);
+        sessionStorage.setItem("cart", JSON.stringify(cart));
+        (event.target as HTMLDivElement).parentElement!.remove();
+    }
 
-    const emptyCheckout = (
-        <div className="Checkout_item">
-            <h1>You dont have any items yet!</h1>
-        </div>
-    );
+    function purchase() {
+
+    }
 
     return (
         <div className="checkout">
             <h1 className="checkout_title">Checkout</h1>
 
             <div className="checkout_item_list">
-                {itemsMap ? itemsMap : emptyCheckout}
+                {items.length > 0 ? items.map((item: any, index: number) => (
+                    <div key={index} className="checkout_item">
+                        <div className="checkout_item_title">{item.name}</div>
+                        <div className="checkout_remove_item" onClick={(e) => {removeFromCart(item, index, e)}}>X</div>
+                        <div className="checkout_item_description">{item.description}</div>
+                        <div className="checkout_item_price">{item.price}</div>
+                        <div className="checkout_item_customizations">Customizations:
+                            {item.customizations.map((option: any ) => (
+                                <p>{option}</p>
+                            ))}
+                        </div>
+                    </div>)) : <h1>You dont have any items yet!</h1>}
             </div>
 
             <div className="checkout_details">
                 <p id="checkout_price">Total<br/>0.00</p>
-                <div className="checkout_purchase_button">Purchase</div>
+                <div className="checkout_purchase_button" onClick={purchase}>Purchase</div>
             </div>
         </div>)
 }
--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -1,13 +1,11 @@
 import './Shop.css'
 import React, {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
-import {render} from "@testing-library/react";
-import {createPortal, flushSync} from "react-dom";
 
 
 /*
     TODO:
-        If item has customization open up a page for that, otherwise just add to cart.
+        Should "work" but overall could use some tweaks to make it work better and be more functional and easy for the user.
  */
 
 //How Items will be stored and used, List of items will be stringified and stored in db then parsed to read all items
@@ -48,10 +46,60 @@ export default function Shop() {
 
     console.log(JSON.stringify(items));
      */
-    const [items, setItems] = useState<any[]>([]);
+    const [storeItems, setStoreItems] = useState<any[]>([]);
     const [customization, setCustomization] = useState<any[]>([]);
     const [showCustomize, setShowCustomize] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
     const [selectedItem, setSelectedItem] = useState<any>(null);
+
+    function clone<T>(obj: T): T { //To avoid changing the original variable
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+    function showCustomizationMenu(item: any) {
+        if (item.customizations.length > 0) {
+            setShowCustomize(true);
+            setSelectedItem(item)
+            setCustomization(item.customizations)
+        } else {
+            addToCart(item)
+        }
+    }
+
+    function closeCustomization() {
+        setShowCustomize(false);
+        setSelectedItem(null);
+        setCustomization([]);
+    }
+
+    function addToCart(item: any, event?: any, customization?: any) {
+        let purchaseItem = clone(item)
+        if (event != null) {
+            event.preventDefault();
+            if (customization != null) {
+                const formData = new FormData(event.target);
+                purchaseItem.customizations = Object.fromEntries(formData.entries());
+            }
+        }
+
+        let list = JSON.parse(sessionStorage.getItem('cart') as string);
+        if (list != null) {
+            let items: any[] = []
+            items = items.concat(list);
+            items.push(purchaseItem);
+            sessionStorage.setItem('cart', JSON.stringify(items));
+        } else {
+            sessionStorage.setItem('cart', JSON.stringify(purchaseItem));
+        }
+        setShowAlert(true);
+        const timer = setTimeout(() => {
+            setShowAlert(false);
+            clearTimeout(timer)
+        }, 6000)
+    }
+
+    const location = useLocation();
+    const state = location.state?.store || null;
 
     async function renderShop() {
         //request all shop data from backend and load it here
@@ -60,74 +108,44 @@ export default function Shop() {
             document.getElementById('shop_name')!!.innerText = state!['Name']
             document.getElementById('shop_desc')!!.innerText = state!['Description']
             let data = JSON.parse(state!['Items'])
-            setItems(data)
+            setStoreItems(data)
         } else {
-            //Only for testing!!
+            //Only for testing!! Can be changed to tell user the page is empty once finished
             document.getElementById('shop_name')!!.innerText = "This is a test page"
             document.getElementById('shop_desc')!!.innerText = "This is meant for testing the websites functionality"
             let items = []
             items.push(new Item("Pizza", "Fresh delicious Pizza!", 7.99, [new Customization("Toppings", ["Pepperoni", "Cheese", "Sausage"]), new Customization("Stuffed Crust", [])]))
             items.push(new Item("Burger", "A really good burger!", 8.99, [new Customization("Bacon", [])]))
             items.push(new Item("Fries", "A good side for out great burgers!", 2.99, []))
-            setItems(items)
+            setStoreItems(items)
         }
     }
 
-    function addToCart(item: any) {
-        //Check if customization is needed and open page
-        if (item.customizations.length > 0) {
-            setShowCustomize(true);
-            setSelectedItem(item)
-            let customizationData: {[key: string]: string} = {};
-            flushSync(() => { //This is temporary and bad practice, I'm only using this because I dont know how else to yet
-                setCustomization(item.customizations)
-            })
-            customization.forEach((item2: any) => {
-                customizationData[item2.name] = "";
-            })
-        }
-
-        let list = JSON.parse(sessionStorage.getItem('cart') as string);
-        if (list != null) {
-            let items = []
-            items.push(list)
-            items.push(item);
-            sessionStorage.setItem('cart', JSON.stringify(items));
-        } else {
-            sessionStorage.setItem('cart', JSON.stringify(item));
-        }
-    }
-
-    const location = useLocation();
-    const state = location.state?.store || null;
     useEffect(() => {
         renderShop();
     }, []);
-
-    function purchase() {
-        //get order data and move to purchase page
-    }
-
 
     return (
         <div className="shop" id="shop">
             {showCustomize && selectedItem && (
                 <div className="customize">
+                    <div className="customize_cancel_button" onClick={() => {closeCustomization()}}>X</div>
                     <h1>Customize your Item</h1>
                     <h2>{selectedItem.name}</h2>
                     <h5>{selectedItem.description}</h5>
-                    <form id="customize_form">
+                    <form id="customize_form" method="dialog" onSubmit={(e) => {addToCart(selectedItem, e, true); closeCustomization() }}>
                         {customization.map((item: any, index: number) => (
                             <div className="form_customization">
                                 <p>{item.name}</p>
-                                {item.options ?
-                                    (<select className="form_select" key={index}>{item.options.map((option: any, op_index: number) => {
-                                            <option className="form_select_option" value={option} key={op_index}>testing</option>
-                                        })}</select>
-                                    ) : (<label className="form_bool_label"><input className="form_choice form_bool" type="checkbox" key={index}/>{item.name}</label> )}
+                                {item.options.length > 0 ?
+                                    (<select className="form_select" key={index} name={item.name}>
+                                            {item.options.map((option: any, op_index: number) => (
+                                            <option className="form_select_option" value={option} key={op_index}>{option}</option>))}
+                                    </select>
+                                    ) : (<><input type="hidden" value="0" name={item.name}/><input className="form_choice form_bool" type="checkbox" value="1" key={index} name={item.name}/></>)}
                             </div>
                         ))}
-                        <input type="submit" value="Add to Cart" className="add_to_cart" onSubmit={purchase}/>
+                        <input type="submit" value="Add to Cart" className="add_to_cart"/>
                     </form>
                 </div>
             )}
@@ -143,15 +161,16 @@ export default function Shop() {
 
             <div className="shop_items_container" id="shop_items_container">
                 <div className="shop_section"><div className="section_title">Item Section</div><div className="shop_divider"/></div>
-                {items.map((item, index) => (
+                {storeItems.map((item, index) => (
                     <div className="shop_item" key={index}>
                         <div className="shop_item_title">{item.name}</div>
                         <div className="shop_item_description">{item.description}</div>
                         <div className="shop_item_price">{item.price}</div>
-                        <div className="shop_item_buy" onClick={() => {addToCart(item)}} key={item.name}>Add to Cart</div>
+                        <div className="shop_item_buy" onClick={() => {showCustomizationMenu(item)}} key={item.name}>Add to Cart</div>
                     </div>
                 ))}
             </div>
+            {showAlert && (<div className="shop_added_popup">Item was added to cart!</div>)}
         </div>
     )
 }
